5f5d5f9314ace1c0cf9c03ccfe453f54e947e1c6
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-11 23:45:31 +0300

    initial project set up

---
+++ src/test/java/com/example/SparingMeals/SparingMealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SparingMeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparingMealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
