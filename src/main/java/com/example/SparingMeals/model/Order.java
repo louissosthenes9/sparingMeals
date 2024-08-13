8bf83977b8bdcb983b77c36451a088b04ed1c311
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-14 01:37:41 +0300

    order models

---
+++ src/main/java/com/example/SparingMeals/model/Order.java
@@ -0,0 +1,43 @@
+package com.example.SparingMeals.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private User customer;
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+
+    private String orderStatus;
+
+    private Date createdAt;
+    @ManyToOne
+    private Address deliveryAddress;
+    // private Payment payment
+    @OneToMany
+    private List<OrderItem> items;
+
+    private int totalPrice;
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/SparingMeals/model/OrderItem.java
@@ -0,0 +1,26 @@
+package com.example.SparingMeals.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Food food;
+
+    private Long totalPrice;
+
+    @ElementCollection
+    private List<String> ingredients;
+}
