b29ac3eee7172b191ed26fc93d861488c9a1e7c9
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-14 01:38:25 +0300

    user model and enum

---
+++ src/main/java/com/example/SparingMeals/model/USER_ROLE.java
@@ -0,0 +1,7 @@
+package com.example.SparingMeals.model;
+
+public enum USER_ROLE {
+    ROLE_CUSTOMER,
+    ROLE_RESTAURANT_OWNER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/example/SparingMeals/model/User.java
@@ -0,0 +1,39 @@
+package com.example.SparingMeals.model;
+
+import com.example.SparingMeals.DTO.RestaurantDTO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private String fullName;
+    private String email;
+    private String password;
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDTO> favourites = new ArrayList();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Address> addresses =  new ArrayList<>();
+
+
+
+}
