ece01825ac53924d8391f49680277fd998468aaa
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-14 01:38:09 +0300

    ingredients models

---
+++ src/main/java/com/example/SparingMeals/model/IngredientsItem.java
@@ -0,0 +1,32 @@
+package com.example.SparingMeals.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class IngredientsItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private IngredientCategory Category;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private boolean inStake = true;
+
+
+
+}
