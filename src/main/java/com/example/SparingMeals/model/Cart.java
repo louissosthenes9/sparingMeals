aab8af9a31417567e661dc707dc1a03dd386cfcf
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-14 14:09:59 +0300

    cart models added

---
+++ src/main/java/com/example/SparingMeals/model/Cart.java
@@ -0,0 +1,31 @@
+package com.example.SparingMeals.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.nio.MappedByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User customer;
+
+    private Long total;
+
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<CartItem> item = new ArrayList<>();
+
+
+
+}
---
+++ src/main/java/com/example/SparingMeals/model/CartItem.java
@@ -0,0 +1,37 @@
+package com.example.SparingMeals.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+
+    @ElementCollection
+    private List<String> ingredients;
+
+    private Long totalPrice;
+
+
+}
+
+
