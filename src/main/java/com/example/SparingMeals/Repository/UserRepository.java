96628051ab9e6b8e042a42d7cbde574180af30a2
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-15 16:02:41 +0300

    initial repository settings

--- src/main/java/com/example/SparingMeals/Repository/UserRepository.java
+++ src/main/java/com/example/SparingMeals/Repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.example.SparingMeals.Repository;public interface UserRepository {
+package com.example.SparingMeals.Repository;
+
+import com.example.SparingMeals.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+        public User findByEmail(String email);
+
+
 }
