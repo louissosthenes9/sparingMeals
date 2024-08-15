954a76024ddbbcdb3a6161365b0bdda22e0a2274
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-15 22:02:59 +0300

    jwt const

---
+++ src/main/java/com/example/SparingMeals/Config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.example.SparingMeals.Config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY ="MTLDDFDLSJSLAP3IQ=EALSIER";
+
+    public static  final  String JWT_HEADER = "Authorization";
+}
