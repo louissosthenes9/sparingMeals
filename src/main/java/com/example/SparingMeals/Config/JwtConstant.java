80bf9986fd316e6b0b32618b91173fc299ed903d
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-20 23:20:28 +0300

    jwt provider added

--- src/main/java/com/example/SparingMeals/Config/JwtConstant.java
+++ src/main/java/com/example/SparingMeals/Config/JwtConstant.java
@@ -3,6 +3,6 @@ package com.example.SparingMeals.Config;
 public class JwtConstant {
 
     public static final String SECRET_KEY ="MTLDDFDLSJSLAP3IQ=EALSIER";
-
     public static  final  String JWT_HEADER = "Authorization";
+    public static final long EXPIRATION_TIME = 3600 * 1000;
 }
