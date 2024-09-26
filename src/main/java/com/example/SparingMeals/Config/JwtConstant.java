829524c0a80339187384d3d8423735d77c769a75
Louissosthenes9 <louissosthenes9@gmail.com>
2024-09-26 23:33:59 +0300

    more secure jwt

--- src/main/java/com/example/SparingMeals/Config/JwtConstant.java
+++ src/main/java/com/example/SparingMeals/Config/JwtConstant.java
@@ -2,7 +2,7 @@ package com.example.SparingMeals.Config;
 
 public class JwtConstant {
 
-    public static final String SECRET_KEY ="MTLDDFDLSJSLAP3IQ=EALSIER";
+    public static final String SECRET_KEY ="MTLDDFDLSJSLAP3IQ=EA@@222234235LSIERKASY9EAKSYIWERUOQYRQEWYIYE8QI4288984WW+/:'EWUQIYRQEQREQ";
     public static  final  String JWT_HEADER = "Authorization";
     public static final long EXPIRATION_TIME = 3600 * 1000;
 }
