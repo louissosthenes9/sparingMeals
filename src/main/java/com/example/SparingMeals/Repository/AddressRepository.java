b1d35e5d727ff7a9e4b408b696eafbe6426cb93f
Louissosthenes9 <louissosthenes9@gmail.com>
2024-09-28 23:15:12 +0300

    address rep

--- src/main/java/com/example/SparingMeals/Repository/AddressRepository.java
+++ src/main/java/com/example/SparingMeals/Repository/AddressRepository.java
@@ -1,2 +1,9 @@
-package com.example.SparingMeals.Repository;public interface AddressRepository {
+package com.example.SparingMeals.Repository;
+
+import com.example.SparingMeals.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+
+
 }
